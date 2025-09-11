1074eecc958dafb83d0dad599e1f8c93c1864767
Kanishkau4 <kanishkau22@gmail.com>
2025-09-11 12:46:57 +0530

    Add WebhookController for handling webhook requests and integrate with app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from "express";
-import axios from "axios";
-import { APP_CONFIG } from "./config/app.config";
 import { MessageController } from "./controller/message.controller";
+import { WebhookController } from "./controller/webhook.controller";
 
 const app = express();
 app.use(express.json());
 
 const messageController = new MessageController();
+const webhookController = new WebhookController();
+
 
 app.post("/send-message", messageController.sentMessage);
-
-
+app.get("/webhook", webhookController.webhook);
+app.post("/webhook", webhookController.webhookMessage);
+app.get("/health", (req, res) => {
+    res.send("Server is healthy");
+});
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
